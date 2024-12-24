bd88f168f0b62199ef6044d5224b5a733ab8c948
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-12-24 10:16:19 -0500

    feat: Apply styles to grid container

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ function createGridContainer(rows, columns) {
     const gridContainer = document.createElement("div");
     gridContainer.classList.add("grid-container");
     gridContainer.id = "gridContainer";
+
+    gridContainer.style.display = "grid";
+    gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+    gridContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+    gridContainer.style.width = "400px";
+    gridContainer.style.height = "auto";
+
     document.body.appendChild(gridContainer);
     console.log("Added container");
 }
