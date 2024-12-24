0c5a8fcc32fe48dcdf37767daf50cc2d6abed247
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-12-24 10:34:18 -0500

    refactor: Remove the styling lines

--- script.js
+++ script.js
@@ -6,12 +6,6 @@ function createGridContainer(rows, columns) {
     gridContainer.classList.add("grid-container");
     gridContainer.id = "gridContainer";
 
-    gridContainer.style.display = "grid";
-    gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`
-    gridContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
-    gridContainer.style.width = "400px";
-    gridContainer.style.height = "auto";
-
     document.body.appendChild(gridContainer);
     return gridContainer;
 }
@@ -24,11 +18,7 @@ function createGridItems(rows, columns) {
 
     for (let i = 0; i < rows * columns; i++) {
         const gridItem = document.createElement("div");
-        
         gridItem.classList.add("grid-item");
-        gridItem.style.border = "solid 1px black";
-        gridItem.style.width = "50px";
-        gridItem.style.height = "50px";
 
         gridContainer.appendChild(gridItem);
     }
